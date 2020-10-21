8eab54adc6cf07ee9f44910b3d15a8a833bc9434
PechPavlo <61708124+PechPavlo@users.noreply.github.com>
2020-10-21 14:48:19 +0300

    feat: add solving of the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let isItBracket = (item) => {
+    return bracketsConfig.flat().includes(item)
+  };
+
+  let condition = (item) => {
+    let currIndex = bracketsConfig.flat().indexOf(item);
+    let nextStuckIitem = bracketsConfig.flat()[bracketsConfig.flat().indexOf(stuck[stuck.length - 1]) + 1];
+    console.log(nextStuckIitem);
+    stuck.length ? nextStuckIitem === bracketsConfig.flat()[currIndex] ? stuck.pop() : stuck.push(bracketsConfig.flat()[currIndex]) : stuck.push(bracketsConfig.flat()[currIndex]);
+  };
+  let stuck = [];
+  let cleanArr = str.split('').filter(isItBracket);
+
+  cleanArr.forEach(condition);
+
+  return !stuck.length
+
 }
